f2a06db045dd229a2786dddf58a2c1fc1c8d0ed1
Guillaume Baptist <guillaume.baptist@free.fr>
2020-11-13 08:46:57 +0100

    deuxième comit toujours bug

--- Outrun/main.cpp
+++ Outrun/main.cpp
@@ -78,24 +78,63 @@ void drawQuad(RenderWindow& w, Color c, int x1, int y1, int w1, int x2, int y2, 
 			///////draw road////////
 
 
-			for (int n = startPos; n <startPos + 300; n++)
+			//for (int n = startPos; n <startPos + 300; n++)
+			//{
+			//	Line &l = lines[n % N];
+			//	l.project(0,1500,pos);
+
+			//	Color grass = (n / 3) % 2 ? Color(16, 200, 16) : Color(0, 154, 0);
+			//	Color rumble = (n / 3) % 2 ? Color(255, 255, 255) : Color(0, 0, 0);
+			//	Color road = (n / 3) % 2 ? Color(107, 107, 107) : Color(105, 105, 105);
+
+			//	
+ 		//		Line p = lines[(n - 1) % N]; //previous line
+
+			//	drawQuad(app, grass, 0, p.Y, width, 0, l.Y, width);
+			//	drawQuad(app, rumble, p.X, p.Y, p.W * 1.2, l.X, l.Y, l.W * 1.2);
+			//	drawQuad(app, road, p.X, p.Y, p.W, l.X, l.Y, l.W);
+			//	app.display();
+			//}
+
+			for (int n = startPos; n < startPos + 300; n++)
 			{
-				Line &l = lines[n % N];
-				l.project(0,1500,pos);
+				Line& l = lines[n % N];
+			/*	l.project(playerX * roadW - x, camH, startPos * segL - (n >= N ? N * segL : 0));
+				x += dx;
+				dx += l.curve;
+
+				l.clip = maxy;
+				if (l.Y >= maxy) continue;
+				maxy = l.Y;*/
 
 				Color grass = (n / 3) % 2 ? Color(16, 200, 16) : Color(0, 154, 0);
 				Color rumble = (n / 3) % 2 ? Color(255, 255, 255) : Color(0, 0, 0);
 				Color road = (n / 3) % 2 ? Color(107, 107, 107) : Color(105, 105, 105);
 
-				
- 				Line p = lines[(n - 1) % N]; //previous line
+				Line p = lines[(n - 1) % N]; //previous line
 
 				drawQuad(app, grass, 0, p.Y, width, 0, l.Y, width);
 				drawQuad(app, rumble, p.X, p.Y, p.W * 1.2, l.X, l.Y, l.W * 1.2);
 				drawQuad(app, road, p.X, p.Y, p.W, l.X, l.Y, l.W);
-				app.display();
 			}
-			//app.display();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+			app.display();
 		}
 
 		return 0;
